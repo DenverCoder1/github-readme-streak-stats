0a06c26df3a43e7285269982ce8b02f20b033ac2
DenverCoder1 <jonah@freshidea.com>
2020-10-30 03:42:56 +0200

    fetch html concurrently and manually parse html

--- src/index.php
+++ src/index.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once "../vendor/autoload.php";
 require_once "card.php";
-use PHPHtmlParser\Dom;
 
 // set cache to refresh once per day
 $timestamp = gmdate("D, d M Y 23:59:00") . " GMT";
@@ -11,47 +9,120 @@ header("Last-Modified: $timestamp");
 header("Pragma: no-cache");
 header("Cache-Control: no-cache, must-revalidate");
 
-// get array of all dates with the number of contributions
-function getContributionDates($user) : array
+$GLOBALS['contributions'] = array();
+
+$user = $_REQUEST["user"];
+
+$stats = getContributionStats($user);
+
+header("Content-Type: image/svg+xml");
+
+echo generateCard($stats);
+
+// get all http requests for contributions
+function getContributionGraphs($user): void
 {
-    $dom = new Dom;
     $startYear = getYearJoined($user);
     $currentYear = (int) date("Y");
-    $contributions = array();
-    // check contributions for every year
+    // build a list of individual requests
+    $urls = [];
     for ($i = $startYear; $i <= $currentYear; $i++) {
         // set end date (leave parameter out for current year)
         $to = $i < $currentYear ? "?to=" . date("${i}-m-d") : "";
-        // load contributions graph
-        $dom->loadFromUrl("https://github.com/users/${user}/contributions${to}");
-        // find rectangles in contributions graph
-        $rectangles = $dom->find("rect");
-        // add the dates and contribution counts to the array
-        foreach ($rectangles as $rect) {
-            $date = $rect->getAttribute("data-date");
-            $count = (int) $rect->getAttribute("data-count");
-            $contributions[$date] = $count;
+        $urls[$i] = curl_init();
+        curl_setopt($urls[$i], CURLOPT_AUTOREFERER, true);
+        curl_setopt($urls[$i], CURLOPT_HEADER, false);
+        curl_setopt($urls[$i], CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($urls[$i], CURLOPT_URL, "https://github.com/users/${user}/contributions${to}");
+        curl_setopt($urls[$i], CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($urls[$i], CURLOPT_VERBOSE, false);
+        curl_setopt($urls[$i], CURLOPT_SSL_VERIFYPEER, false);
+    }
+    // build multi-curl handle
+    $multi = curl_multi_init();
+    foreach ($urls as $url) {
+        curl_multi_add_handle($multi, $url);
+    }
+    // execute queries
+    $running = null;
+    do {
+        curl_multi_exec($multi, $running);
+    } while ($running);
+    //close the handles
+    foreach ($urls as $url) {
+        curl_multi_remove_handle($multi, $url);
+    }
+    curl_multi_close($multi);
+    // collect responses
+    foreach ($urls as $url) {
+        pushContributions(curl_multi_getcontent($url));
+    }
+}
+
+// get contributions from HTML
+function pushContributions($html): void
+{
+    // split into lines
+    $lines = explode("\n", $html);
+    // add the dates and contribution counts to the array
+    foreach ($lines as $line) {
+        preg_match("/ data-date=\"([0-9\-]{10})\"/", $line, $dateMatch);
+        preg_match("/ data-count=\"(\d+?)\"/", $line, $countMatch);
+        if (isset($dateMatch[1]) && isset($countMatch[1])) {
+            $date = $dateMatch[1];
+            $count = (int) $countMatch[1];
+            $GLOBALS['contributions'][$date] = $count;
         }
     }
-    return $contributions;
+}
+
+// get array of all dates with the number of contributions
+function getContributionDates($user) : array
+{
+    getContributionGraphs($user);
+    return $GLOBALS['contributions'];
+}
+
+// Get the contents of a URL
+function curl_get_contents($url): string
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_VERBOSE, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
 }
 
 // get the first year a user contributed
 function getYearJoined($user) : int
 {
-    $dom = new Dom;
     // load the user's profile page
-    $dom->loadFromUrl("https://github.com/${user}");
+    $html = curl_get_contents("https://github.com/${user}");
     // find the year links below the contribution graph
-    $years = $dom->find("a.js-year-link");
+    $years = [];
+    $lines = explode("\n", $html);
+    foreach ($lines as $line) {
+        if (strpos($line, "id=\"year-link-2") !== false) {
+            preg_match("/year-link-(\d{4})/", $line, $yearMatch);
+            if (isset($yearMatch[1])) {
+                array_push($years, (int) $yearMatch[1]);
+            }
+        }
+    }
     // check that user page is working
     if (count($years) > 0) {
         // get the inner text of the last year on the page
-        return (int) $years[count($years) - 1]->innerText;
+        return $years[count($years) - 1];
     }
     // no user page found at the URL
     else {
-        die("User does not exist.");
+        die("User info could not be found.");
     }
 }
 
@@ -108,11 +179,3 @@ function getContributionStats($user) : array
     }
     return $stats;
 }
-
-$user = $_REQUEST["user"];
-
-$stats = getContributionStats($user);
-
-header("Content-Type: image/svg+xml");
-
-echo generateCard($stats);
