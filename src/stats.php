407baaf334776ed047ab8bd92a826f5509512bb9
DenverCoder1 <jonah@freshidea.com>
2021-01-26 11:21:36 +0200

    🚦 Avoiding ratelimit with authentication

--- src/stats.php
+++ src/stats.php
@@ -74,7 +74,10 @@ function curl_get_contents($url): string
 {
     $ch = curl_init();
     $user_agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
+    $username = getenv('USERNAME');
+    $token = getenv('TOKEN');
     curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+    curl_setopt($ch, CURLOPT_USERPWD, "$username:$token");
     curl_setopt($ch, CURLOPT_AUTOREFERER, true);
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
