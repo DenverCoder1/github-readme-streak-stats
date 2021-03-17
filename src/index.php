51e01c51a195f2b9464fbfe6c827c00acd744ebf
Jonah Lawrence <jonah@freshidea.com>
2021-03-17 20:08:09 +0200

    💻 Added Demo Site

--- src/index.php
+++ src/index.php
@@ -1,8 +1,16 @@
 <?php
 
+// load functions
 require_once "stats.php";
 require_once "card.php";
-file_exists("config.php") && include "config.php";
+
+// load config
+if (file_exists("config.php")) {
+    require_once "config.php";
+}
+else {
+    die(generateErrorCard("/src/config.php was not found. Check Contributing.md for details."));
+}
 
 // set cache to refresh once per day
 $timestamp = gmdate("D, d M Y 23:59:00") . " GMT";
