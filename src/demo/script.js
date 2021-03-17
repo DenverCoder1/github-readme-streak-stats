6f1c7a5f841d4e3bf4876c78a301edeefc390a76
Jonah Lawrence <jonah@freshidea.com>
2021-03-18 01:48:10 +0200

    🎨 Add theme customizations to demo

--- src/demo/script.js
+++ src/demo/script.js
@@ -11,7 +11,7 @@ let preview = {
     const params = Array.from(document.querySelectorAll(".param")).reduce(
       (acc, next) => {
         let obj = { ...acc };
-        obj[next.id] = next.value;
+        obj[next.id] = next.value.replace(/#/g, "");
         return obj;
       },
       {}
@@ -32,6 +32,77 @@ let preview = {
     document.querySelector(".output img").src = demoImageURL;
     // update markdown
     document.querySelector(".md code").innerText = md;
+  },
+  addProperty: function (property) {
+    const selectElement = document.querySelector("#properties");
+    // if no property passed, get the currently selected property
+    if (!property) {
+      property = selectElement.value;
+    }
+    if (!selectElement.disabled) {
+      // disable option in menu
+      Array.prototype.find.call(
+        selectElement.options,
+        (o) => o.value == property
+      ).disabled = true;
+      // select first unselected option
+      const firstAvailable = Array.prototype.find.call(
+        selectElement.options,
+        (o) => !o.disabled
+      );
+      if (firstAvailable) {
+        firstAvailable.selected = true;
+      } else {
+        selectElement.disabled = true;
+      }
+      // label
+      const label = document.createElement("label");
+      label.innerText = property;
+      label.setAttribute("data-property", property);
+      // color picker
+      const input = document.createElement("input");
+      input.type = "color";
+      input.className = "param";
+      input.id = property;
+      input.name = property;
+      input.value = "#dd2727"; // default red color
+      input.setAttribute("data-property", property);
+      // removal button
+      const minus = document.createElement("button");
+      minus.className = "minus btn";
+      minus.setAttribute(
+        "onclick",
+        "return preview.removeProperty(this.getAttribute('data-property'));"
+      );
+      minus.innerText = "−";
+      minus.setAttribute("data-property", property);
+      // add elements
+      const parent = document.querySelector(".advanced .parameters");
+      parent.appendChild(label);
+      parent.appendChild(input);
+      parent.appendChild(minus);
+      // update and exit
+      this.update();
+    }
+    return false;
+  },
+  removeProperty: function (property) {
+    const parent = document.querySelector(".advanced .parameters");
+    const selectElement = document.querySelector("#properties");
+    // remove all elements for given property
+    parent
+      .querySelectorAll(`[data-property="${property}"]`)
+      .forEach((x) => parent.removeChild(x));
+    // enable option in menu
+    const option = Array.prototype.find.call(
+      selectElement.options,
+      (o) => o.value == property
+    );
+    selectElement.disabled = false;
+    option.disabled = false;
+    // update and exit
+    this.update();
+    return false;
   },
 };
 
@@ -68,12 +139,25 @@ document.addEventListener("click", () => preview.update(), false);
 window.addEventListener(
   "load",
   () => {
-    // set input boxes to match URL parameters
-    new URLSearchParams(window.location.search).forEach(
-      (val, key) => (document.querySelector(`#${key}`).value = val)
-    );
-    // update previews
-    preview.update();
+    // check if URL parameters are set
+    if (window.location.search) {
+      // set input boxes to match URL parameters
+      new URLSearchParams(window.location.search).forEach((val, key) => {
+        let paramInput = document.querySelector(`#${key}`);
+        if (paramInput) {
+          // set parameter value
+          paramInput.value = val;
+        } else {
+          // add advanced property
+          document.querySelector("details.advanced").open = true;
+          preview.addProperty(key);
+          paramInput = document.querySelector(`#${key}`);
+          paramInput.value = val;
+        }
+      });
+      // update previews
+      preview.update();
+    }
   },
   false
 );
