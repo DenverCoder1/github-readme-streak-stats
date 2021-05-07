3b85c5d9d4eedd89b48c19e07c50dcdcedc701b4
schmelto <30869493+schmelto@users.noreply.github.com>
2021-05-07 05:16:34 +0200

    feat: add prefers-color-scheme

--- src/demo/js/toggle-dark.js
+++ src/demo/js/toggle-dark.js
@@ -1,17 +1,30 @@
+const icon = document.querySelector(".darkmode i");
+if (window.matchMedia("(prefers-color-scheme: dark)").matches == true) {
+  darkmode();
+} else {
+  lightmode();
+}
+
 function toggleTheme() {
-  const icon = document.querySelector(".darkmode i");
-  /* dark mode on */
   if (document.body.getAttribute("data-theme") !== "dark") {
-    icon.className = "gg-sun";
-    setCookie("darkmode", "on", 9999);
-    document.body.setAttribute("data-theme", "dark");
+    /* dark mode on */
+    darkmode();
+  } else {
+    /* dark mode off */
+    lightmode();
   }
-  /* dark mode off */
-  else {
-    icon.className = "gg-moon";
-    setCookie("darkmode", "off", 9999);
-    document.body.removeAttribute("data-theme");
-  }
+}
+
+function darkmode() {
+  icon.className = "gg-sun";
+  setCookie("darkmode", "on", 9999);
+  document.body.setAttribute("data-theme", "dark");
+}
+
+function lightmode() {
+  icon.className = "gg-moon";
+  setCookie("darkmode", "off", 9999);
+  document.body.removeAttribute("data-theme");
 }
 
 function setCookie(cname, cvalue, exdays) {
