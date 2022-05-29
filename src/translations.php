66f7fa878408a2e63480b162aa35453141d06ea9
MagicLike <magiclike@tuta.io>
2022-05-29 10:48:56 +0200

    feat: Added German translation (#240)

--- src/translations.php
+++ src/translations.php
@@ -3,16 +3,19 @@
 return [
     "currentStreak" => [
         "en" => "Current Streak",
+        "de" => "Aktuelle Serie",
         "es" => "Racha Actual",
         "ja" => "現在のストリーク",
     ],
     "totalContributions" => [
         "en" => "Total Contributions",
+        "de" => "Gesamte Beiträge",
         "es" => "Todas Contribuciones",
         "ja" => "総ｺﾝﾄﾘﾋﾞｭｰｼｮﾝ数",
     ],
     "longestStreak" => [
         "en" => "Longest Streak",
+        "de" => "Längste Serie",
         "es" => "Racha Más Larga",
         "ja" => "最長のストリーク",
     ],
