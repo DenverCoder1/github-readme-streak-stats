fafd60cff6ae65d1860309921cd49a84028464e2
Abraham Murciano <abrahammurciano@gmail.com>
2022-05-29 10:53:55 +0300

    feat: Spanish locale 🇪🇸🇪🇸🇪🇸 (#239)

--- src/translations.php
+++ src/translations.php
@@ -3,14 +3,17 @@
 return [
     "currentStreak" => [
         "en" => "Current Streak",
+        "es" => "Racha Actual",
         "ja" => "現在のストリーク",
     ],
     "totalContributions" => [
         "en" => "Total Contributions",
+        "es" => "Todas Contribuciones",
         "ja" => "総ｺﾝﾄﾘﾋﾞｭｰｼｮﾝ数",
     ],
     "longestStreak" => [
         "en" => "Longest Streak",
+        "es" => "Racha Más Larga",
         "ja" => "最長のストリーク",
     ],
 ];
