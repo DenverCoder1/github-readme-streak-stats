57bef6bcca4315547de6fd27463592f1ce73e921
MagicLike <contact.magiclike@protonmail.com>
2022-05-29 09:27:58 +0200

    Added German translation

--- src/translations.php
+++ src/translations.php
@@ -3,14 +3,17 @@
 return [
     "currentStreak" => [
         "en" => "Current Streak",
+        "de" => "Aktuelle Serie",
         "ja" => "現在のストリーク",
     ],
     "totalContributions" => [
         "en" => "Total Contributions",
+        "de" => "Gesamte Beiträge",
         "ja" => "総ｺﾝﾄﾘﾋﾞｭｰｼｮﾝ数",
     ],
     "longestStreak" => [
         "en" => "Longest Streak",
+        "de" => "Längste Serie",
         "ja" => "最長のストリーク",
     ],
 ];
