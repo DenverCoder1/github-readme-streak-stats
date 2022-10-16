83d9e17d5b5852fe8d5c0a6491f2a91e4162b042
ABIPRAVI <65100038+pravee42@users.noreply.github.com>
2022-10-16 11:44:06 +0530

    Added Tamil Translation (#334)
    Co-authored-by: Jonah Lawrence <jonah@freshidea.com>

--- src/translations.php
+++ src/translations.php
@@ -118,6 +118,12 @@ return [
         "Longest Streak" => "Самая длинная серия",
         "Present" => "Сейчас",
     ],
+    "ta" => [
+        "Total Contributions" => "மொத்த பங்களிப்புகள்",
+        "Current Streak" => "மிக சமீபத்திய பங்களிப்புகள்",
+        "Longest Streak" => "நீண்ட தொடர்ச்சியான பங்களிப்புகள்",
+        "Present" => "இன்றுவரை",
+    ],
     "tr" => [
         "Total Contributions" => "Toplam Katkı",
         "Current Streak" => "Güncel Seri",
