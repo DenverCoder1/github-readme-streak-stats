4aa6fdea62f2da2a749c21471463f1384eac7a6f
R0BL0XIAND3M0 <55234806+DsGamer123@users.noreply.github.com>
2022-12-02 02:01:52 -0500

    Fix up the Bangla translations.

--- src/translations.php
+++ src/translations.php
@@ -36,10 +36,10 @@ return [
     // Locales below are sorted alphabetically
     "bn" => [
         "Total Contributions" => "মোট অবদান",
-        "Current Streak" => "কারেন্ট ডোরা",
-        "Longest Streak" => "দীর্ঘতম ডোরা",
-        "Week Streak" => "সাপ্তাহিক ডোরা",
-        "Longest Week Streak" => "দীর্ঘতম সাপ্তাহিক ডোরা",
+        "Current Streak" => "কারেন্ট স্ট্রীক",
+        "Longest Streak" => "দীর্ঘতম স্ট্রিক",
+        "Week Streak" => "সপ্তাহ স্ট্রীক",
+        "Longest Week Streak" => "দীর্ঘতম সপ্তাহ স্ট্রিক",
         "Present" => "বর্তমান",
     ],
     "da" => [
