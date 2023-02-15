b62b125667cc1b00c698db2fe46e51947b810ca7
Jonah Lawrence <jonah@freshidea.com>
2023-02-14 21:41:25 -0700

    fix: workaround for transparent background in PNG mode (#453)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/card.php
+++ src/card.php
@@ -494,6 +494,15 @@ function convertSvgToPng(string $svg): string
     $svg = preg_replace("/(animation: currstreak[^;'\"]+)/m", "font-size: 28px;", $svg);
     $svg = preg_replace("/<a \X*?>(\X*?)<\/a>/m", '\1', $svg);
 
+    // replace all fully transparent colors in fill or stroke with "none"
+    // this is a workaround for what seems to be a bug in inkscape where rgba alpha values are ignored
+    // TODO: find a way to make partially transparent colors work (eg. #ffffff50)
+    $svg = preg_replace(
+        "/(fill|stroke)=['\"](?:#[0-9a-fA-F]{6}00|#[0-9a-fA-F]{3}0|transparent)['\"]/m",
+        '\1="none"',
+        $svg
+    );
+
     // escape svg for shell
     $svg = escapeshellarg($svg);
 
