6e92f30e313fb4c791b2dafc5a4f4472a28a90b3
Jonah Lawrence <jonah@freshidea.com>
2023-02-15 06:40:21 +0200

    refactor regex - non capturing group

--- src/card.php
+++ src/card.php
@@ -498,7 +498,7 @@ function convertSvgToPng(string $svg): string
     // this is a workaround for what seems to be a bug in inkscape where rgba alpha values are ignored
     // TODO: find a way to make partially transparent colors work (eg. #ffffff50)
     $svg = preg_replace(
-        "/(fill|stroke)=['\"](#[0-9a-fA-F]{6}00|#[0-9a-fA-F]{3}0|transparent)['\"]/m",
+        "/(fill|stroke)=['\"](?:#[0-9a-fA-F]{6}00|#[0-9a-fA-F]{3}0|transparent)['\"]/m",
         '\1="none"',
         $svg
     );
