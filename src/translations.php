6080028569d2fc4ac87921c33235d27a54d6376a
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-27 16:23:56 +0300

    feat: Added Hungarian translations (#539)
    * Hungarian translation added.
    
    #236
    
    * Excluding added for Hungarian translation.

--- src/translations.php
+++ src/translations.php
@@ -163,6 +163,15 @@ return [
         "Longest Week Streak" => "pi long tras semèn",
         "Present" => "Prezan",
     ],
+    "hu" => [
+        "Total Contributions" => "Összes hozzájárulás",
+        "Current Streak" => "Jelenlegi sorozat",
+        "Longest Streak" => "Leghosszabb sorozat",
+        "Week Streak" => "Heti sorozat",
+        "Longest Week Streak" => "Leghosszabb heti sorozat",
+        "Present" => "Jelen",
+        "Excluding" => "Kivéve",
+    ],
     "hy" => [
         "Total Contributions" => "Ընդհանուր\nներդրումը",
         "Current Streak" => "Ընթացիկ շարք",
