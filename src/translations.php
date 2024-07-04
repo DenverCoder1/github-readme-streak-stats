fbd03c5f0240da78e0d53cd7b158842e16ddf1e7
Jerzy Kruszewski <50929198+JerzyKruszewski@users.noreply.github.com>
2024-07-04 22:39:52 +0200

    feat: Added missing Polish (pl) translation for Excluding {days} (#699)

--- src/translations.php
+++ src/translations.php
@@ -332,6 +332,7 @@ return [
         "Week Streak" => "Seria Tygodni",
         "Longest Week Streak" => "Najdłuższa Seria Tygodni",
         "Present" => "Dziś",
+        "Excluding {days}" => "Wykluczono {days}",
     ],
     "ps" => [
         "rtl" => true,
