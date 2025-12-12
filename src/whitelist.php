f94debac181257e79c9aa7cb59424cb8c35d1b10
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-12-12 11:45:04 +0100

    fix: WHITELIST environment variable

--- src/whitelist.php
+++ src/whitelist.php
@@ -8,6 +8,7 @@
  */
 function isWhitelisted(string $user): bool
 {
-    $whitelist = array_map("trim", array_filter(explode(",", $_SERVER["WHITELIST"] ?? "")));
+    $whitelistStr = $_SERVER["WHITELIST"] ?? $_ENV["WHITELIST"] ?? getenv("WHITELIST") ?? "";
+    $whitelist = array_map("trim", array_filter(explode(",", $whitelistStr)));
     return empty($whitelist) || in_array($user, $whitelist, true);
 }
