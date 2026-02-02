a545b6f966803790a54f619a93cf31402ce15914
FlavioKde <flaviodavirro@gmail.com>
2026-02-02 23:14:29 +0100

    refactor(hello): update code api endpoint

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,14 @@
 export default function handler(req, res) {
-  res.status(200).json(
-    { message: "Hello, world from VERCEL API!",
+  res.status(200).json({ 
+      status: "ok",
+      service: "github-readme-streak-stats",
+      version: "0.1.0",
       timestamp: new Date().toISOString(),
+      endpoints: [
+        stats="/api/stats? user= username",
+        hello="/api/hello"
+      ],
+      message: "Hello, world from VERCEL API!",
+      
      });
 }
