3d349ff1c8355d5ea4e8ea3a9c033b5f104ed441
Prashanth Kumar G <prashantkumarrrg777@outlook.com>
2026-07-12 00:48:55 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
 declare(strict_types=1);
 
 // load functions
