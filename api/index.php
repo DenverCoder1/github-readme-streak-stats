67823c933adf759a53f11b179959fad30e4dde4b
Prashanth Kumar G <prashantkumarrrg777@outlook.com>
2026-07-14 00:45:32 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
 <?php
+// Turn off all error reporting completely
 error_reporting(0);
+ini_set('display_errors', '0');
+
+// Start output buffering to capture any accidental output
+ob_start();
 
 declare(strict_types=1);
 
