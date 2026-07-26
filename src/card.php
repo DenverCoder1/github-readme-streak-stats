68946bef3b6881af969ae7c2a706243e3cf914e6
Jonah Lawrence <jonah@freshidea.com>
2026-07-26 16:26:37 +0300

    fix: Resolve nullable parameter deprecation issue in PHP 8.4 (#925)

--- src/card.php
+++ src/card.php
@@ -367,7 +367,7 @@ function formatNumber(float $num, string $localeCode, bool $useShortNumbers): st
  *
  * @throws InvalidArgumentException If a locale does not exist
  */
-function generateCard(array $stats, array $params = null): string
+function generateCard(array $stats, ?array $params = null): string
 {
     $params = $params ?? $_REQUEST;
 
@@ -615,7 +615,7 @@ function generateCard(array $stats, array $params = null): string
  * @param array<string,string>|NULL $params Request parameters
  * @return string The generated SVG error card
  */
-function generateErrorCard(string $message, array $params = null): string
+function generateErrorCard(string $message, ?array $params = null): string
 {
     $params = $params ?? $_REQUEST;
 
@@ -807,7 +807,7 @@ function convertSvgToPng(string $svg, int $cardWidth, int $cardHeight): string
  * @param int $errorCode The HTTP error code (used for JSON responses)
  * @return array The Content-Type header and the response body, and status code in case of an error
  */
-function generateOutput(string|array $output, array $params = null, int $errorCode = 200): array
+function generateOutput(string|array $output, ?array $params = null, int $errorCode = 200): array
 {
     $params = $params ?? $_REQUEST;
 
